3aaba7c5d1863aafdaaf998322f6555d7e3d26d4
Sean Worrell <spworrell@gmail.com>
2023-09-09 09:21:54 -0400

    done

--- server.js
+++ server.js
@@ -16,29 +16,3 @@ app.get("/notes", (req, res) => res.sendFile(__dirname + "/public/notes.html"));
 
 // start server
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
-
-//APIS
-// const getNotes = () =>
-//   fetch('/api/notes', {
-//     method: 'GET',
-//     headers: {
-//       'Content-Type': 'application/json',
-//     },
-//   });
-
-// const saveNote = (note) =>
-//   fetch('/api/notes', {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json',
-//     },
-//     body: JSON.stringify(note),
-//   });
-
-// const deleteNote = (id) =>
-//   fetch(`/api/notes/${id}`, {
-//     method: 'DELETE',
-//     headers: {
-//       'Content-Type': 'application/json',
-//     },
-//   });
