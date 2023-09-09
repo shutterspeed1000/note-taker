c2ccaac227cdbc02ea2a996adf497a7c1917fb64
Sean Worrell <spworrell@gmail.com>
2023-09-08 23:25:44 -0400

    its alive

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,34 +1,49 @@
-const express = require('express');
-const fs = require(`fs`)
-const app = express()
+const express = require("express");
+const fs = require(`fs`);
+const app = express();
 
 app.use(express.json());
 
-const notes = require('../db/db.json');
+const notes = require("../db/db.json");
 
 // GET all notes in DB
-app.get('/notes', (req, res) => res.json(notes))
+app.get("/notes", (req, res) => res.json(notes));
 
- app.post('/notes', (req, res) => {
+// POST new note to DB and write file
 
-    console.log(notes)  
+app.post("/notes", (req, res) => {
+  let newNote = req.body;
 
-    console.log(req.body)
+  //set note id based on number of items in array
+  newNote.id = notes.length + 1;
+  notes.push(newNote);
 
-    notes.push(req.body)
-   
-console.log(notes)
-
-fs.writeFileSync('./db/db.json',JSON.stringify(notes),function(err) {
-   if (err) {
+  fs.writeFile("./db/db.json", JSON.stringify(notes), function (err) {
+    if (err) {
       console.log(err);
     } else {
-      console.log('File written successfully');
+      console.log("File written successfully");
     }
- });
-      
-   })
+  });
+  // load new notes/all notes after DB update
+  res.json(notes);
+});
 
+app.delete("/notes/:id", (req, res) => { 
+
+   notes.splice(req.params.id - 1, 1);
+
+   fs.writeFile("./db/db.json", JSON.stringify(notes), function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("File written successfully");
+      }
+    });
+
+   res.json(notes);
+
+})
 
 
 
